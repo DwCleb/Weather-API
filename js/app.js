3583db5109628acbae31313b8febed08a2ba9539
Cleber Junior <dwcleb@gmail.com>
2018-07-24 21:44:45 -0300

    Stage 3

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ var current = false;
 
 function forecast(id) {
   var appKey = "5cfdaac4c096563bcd39c629ac87f25a";
+  var check = document.getElementById("temperature");
+  if(check.checked) var unit = "imperial";
+  else var unit = "metric";
+
   $.ajax({
     type: 'GET',
     dataType: 'json',
@@ -13,20 +17,28 @@ function forecast(id) {
     data: {
       id: id,
       appid: appKey,
+      units: unit,
     },
     success: function (response) {
       console.log(response); // geral
+      if (unit == "imperial") tempM = " °F";
+      else tempM = " °C"; 
       //gerar as 5 datas
       var media = 0;
       var dates = [];
       var temps = [];
+      var temps_min = [];
+      var temps_max = [];
       var descs = [];
       var icons = [];
 
       //obtenção das datas para consulta
       for (let index = 0; index < response.list.length; index++) {
         date = response.list[index].dt_txt.split(" ");
-        dates[index] = date[0];
+        if ((date[1] >= "06:00:00") && (date[1] <= "17:00:00")){
+          dates[index] = date[0];
+          
+        }
       }
       //tratamento das datas
       var newDates = dates.filter(function (here, i) {
@@ -35,46 +47,36 @@ function forecast(id) {
       //./
       
       //obtenção da temperatura, descriçãoe icone atráves da data
+      subIndex = 0;
       for (let index = 0; index < response.list.length; index++) {
         date = response.list[index].dt_txt.split(" ");
         if(newDates.indexOf(date[0]) > -1){
-          temps[index] = response.list[index].main.temp;
-          descs[index] = response.list[index].weather[0].description;
-          icons[index] = response.list[index].weather[0].icon;
-          // exclui a data já usada
-          var delet = newDates.indexOf(date[0]);
-          if ( delet > -1) {
-            newDates.splice(delet, 1);
+          if ((date[1] >= "06:00:00") && (date[1] <= "17:00:00")){
+            temps[subIndex] = response.list[index].main.temp;
+            temps_min[subIndex] = response.list[index].main.temp_min;
+            temps_max[subIndex] = response.list[index].main.temp_max;
+            descs[subIndex] = response.list[index].weather[0].description;
+            icons[subIndex] = response.list[index].weather[0].icon;
+            // exclui a data já usada
+            subIndex++;
+            var delet = newDates.indexOf(date[0]);
+            if ( delet > -1) {
+              newDates.splice(delet, 1);
+            }
+            //console.log(date[1]);
           }
           //./
         }
       }
       //./
-
-      //tratamento das temperaturas
-      var newTemps = temps.filter(function (here, i) {
-        return temps.indexOf(here) == i;
-      });
-      temps = newTemps;
-      //./
-      
-      //tratamento das descrições
-      var newdDescs = descs.filter(function (here, i) {
-        return descs.indexOf(here) == i;
-      });
-      descs = newdDescs;
-      //./
-      //tratamento das descrições
-      var newdIcons = icons.filter(function (here, i) {
-        return icons.indexOf(here) == i;
-      });
-      icons = newdIcons;
-      //./
-
+     
       // reobtenção das datas que foram utilizadas e perdidas da memória
       for (let index = 0; index < response.list.length; index++) {
         date = response.list[index].dt_txt.split(" ");
-        dates[index] = date[0];
+        if ((date[1] >= "06:00:00") && (date[1] <= "17:00:00")){
+          dates[index] = date[0];
+          console.log(date[1]);
+        }
       }
       //tratamento das datas
       var dates = dates.filter(function (here, i) {
@@ -94,11 +96,13 @@ function forecast(id) {
       table += "</thead>";
       table += " <tbody>";
       //construção da tabela
-      for (let index = 0; index < temps.length; index++) {
+      for (let index = 0; index < dates.length; index++) {
         table += "<tr>";
         table += "<th scope=\"row\">" + (index + 1) + "</th> ";
         table += "<td><img src=\"https://openweathermap.org/img/w/" + icons[index] + ".png\" width=\"50\" height=\"50\" ></td> ";
-        table += "<td><h4><strong>" + convert(temps[index]) + " °F</strong></h4><span style=\"font-size: 10pt;\">" + descs[index] + "</span></td> ";
+        table += "<td><h4><strong>" + temps[index] + tempM + " </strong></h4>";
+        table += "<span style=\"font-size: 10pt;\">" + descs[index] + "</span><br>";
+        table += "<span style=\"font-size: 8pt;\">Min <strong>"  + temps_min[index] + tempM + "</strong> | Max <strong>" + temps_max[index] + tempM + "</strong></span></td> ";
         table += "<td>" + dates[index] + "</td> ";
         table += "</tr> ";
 
@@ -108,7 +112,7 @@ function forecast(id) {
       media = (media/(qtdDays + 1));
       table += "<tr>";
       table += "<td colspan='6'>";
-      table += "<center> Forecast average <strong>" + convert(media) + " °F</strong></center>";
+      table += "<center> Forecast average <strong>" + media.toFixed(2) + tempM + "</strong></center>";
       table += "</td>";
       table += "</tr>";
       table += " </tbody>";
@@ -117,13 +121,14 @@ function forecast(id) {
 
       //Envio das informações
       document.getElementById("infoCurrent").style.display = "none";
+      $("#forecastTable").fadeIn(500);
       document.getElementById("tableWeather").innerHTML = table;
       document.getElementById("selectCity").innerHTML = response.city.name;
       document.getElementById("selectCountry").innerHTML = response.city.country;
       //./
        $("#resultContainer").fadeIn(500);
        $('html, body').animate({
-         scrollTop: $('#footer').offset().top - 50
+         scrollTop: $('#resultContainer').offset().top - 50
        }, "slow");
        
     },
@@ -152,6 +157,9 @@ function forecast(id) {
 function currentD(id) {
   var appKey = "5cfdaac4c096563bcd39c629ac87f25a";
   var img = "https://openweathermap.org/img/w/";
+  var check = document.getElementById("temperature");
+  if(check.checked) var unit = "imperial";
+  else var unit = "metric";
   $.ajax({
     type: 'GET',
     dataType: 'json',
@@ -160,18 +168,24 @@ function currentD(id) {
     data: {
       id: id,
       appid: appKey,
+      units: unit,
     },
     success: function (response) {
       //console.log(response); // geral
+      if (unit == "imperial") tempM = " °F";
+      else tempM = " °C"; 
       document.getElementById("infoCurrent").style.display = "block";
-      document.getElementById("MainTemp").innerHTML = convert(response.main.temp) + " °F";
+      document.getElementById("MainTemp").innerHTML = response.main.temp + tempM;
       document.getElementById("imgWeather").src = img + response.weather[0].icon + ".png";
       document.getElementById("description").innerHTML = response.weather[0].description;
+      document.getElementById("temps").innerHTML = "Min <strong>" + response.main.temp_min + tempM;
+      document.getElementById("temps").innerHTML += "</strong> | Max <strong>" + response.main.temp_max + tempM + "</strong>";
       document.getElementById("selectCity").innerHTML = response.name;
       document.getElementById("selectCountry").innerHTML = response.sys.country;
       $("#resultContainer").fadeIn(500);
+      $("#forecastTable").fadeOut(500);
       $('html, body').animate({
-        scrollTop: $('#footer').offset().top - 50
+        scrollTop: $('#resultContainer').offset().top - 50
       }, "slow");
     },
     error: function (jqXHR, exception) {
@@ -197,7 +211,9 @@ function currentD(id) {
 }
 
 function city() {
-  country = document.getElementById("country").options[document.getElementById("country").selectedIndex].text;
+  country = document.getElementById("country").value;
+  $("#city").fadeOut(500);
+  $("#loadCity").html("<center><img src='load.gif' width='30' height='30'></center>");
   $("#city").html("");
   $.ajax({
     type: 'GET',
@@ -205,8 +221,7 @@ function city() {
     url: 'https://raw.githubusercontent.com/dwcleb/Weather-API---openWeatherMap/master/city.list.min.json',
     async: true,
     success: function (response) {
-      //console.log(response.length);
-      // for (let index = 0; index < response.length; index++) {
+      response = response.sort();
       for (let index = 0; index < response.length; index++) {
         if (response[index].country == country) {
           let cityName = response[index].name;
@@ -216,7 +231,10 @@ function city() {
           $("#city").append(option);
         }
       }
-
+      setTimeout(() => {
+        $("#loadCity").fadeOut(500);
+        $("#city").fadeIn(500);
+      }, 500);
     },
     error: function (jqXHR, exception) {
       msg = "Undefined Error";
@@ -242,20 +260,21 @@ function city() {
 }
 
 function country() {
+  $("#country").html("");
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    url: 'https://raw.githubusercontent.com/dwcleb/Weather-API---openWeatherMap/master/country.list.json',
+    // url: 'https://raw.githubusercontent.com/dwcleb/Weather-API---openWeatherMap/master/country.list.json',
+    url: 'https://restcountries.eu/rest/v2/all',
     async: true,
     success: function (response) {
-      //console.log(response.length);
-      // for (let index = 0; index < response.length; index++) {
-      option = "";
+      //console.log(response);
+      option = "<option value='#'> Select Country</option>";
       var c = [];
       for (let index = 0; index < response.length; index++) {
-        let countryName = response[index];
-        //console.log(countryId);
-        option += "<option>" + countryName + "</option>";
+        let countryName = response[index].name;
+        let alpha2Code = response[index].alpha2Code;
+        option += "<option value=" + alpha2Code + ">" + countryName + "</option>";
         
       }
       $("#country").append(option);
@@ -287,11 +306,14 @@ function country() {
 function currentData() {
   current = true;
   container();
+  $("select[name=country]").val("#");
 }
 
 function forecastData() {
   current = false;
   container();
+  $("select[name=country]").val("#");
+
 }
 
 function container() {
@@ -299,6 +321,7 @@ function container() {
   $("#cityContainer").fadeOut(500);
   $("#VerifyContainer").fadeOut(500);
   $("#resultContainer").fadeOut(500);
+  $("#forecastTable").fadeOut(500);
 }
 
 function cityContainer() {
@@ -312,6 +335,15 @@ function getData() {
   if (current) currentD(id);
   else forecast(id);
 }
+
 function convert(value) {
   return (1.8 * (value - 273) + 32).toFixed(2);
+}
+
+function change() {
+  var check = document.getElementById("temperature");
+  if(check.checked) document.getElementById('tmpType').innerHTML = "Fahrenheit";
+  else document.getElementById('tmpType').innerHTML = "Celsius";
+  $("#resultContainer").fadeOut(500);
+  //$("#container").fadeOut(500);
 }
